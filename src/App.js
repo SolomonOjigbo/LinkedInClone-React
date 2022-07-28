dfc096dd7a2747f8b7aba3ff47c658e66e1bed12
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-28 04:39:21 +0100

    Created the Login component and aaded redux, useSlice reducer to manage user state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { auth } from './components/firebase';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Feeds from './components/Feeds';
 import Header from './components/Header';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import Widgets from './components/Widgets';
+import { login, logout, selectUser } from './features/userSlice';
 
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoURL: userAuth.photoURL,
+        }));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+
   return (
     <div className="App">
-      <Header />
-    <div className='app__body'>
-      <Sidebar />
-      <Feeds />
-      <Widgets />
-
-    </div>
+      {!user ? (
+        <Login />
+      ) : (<div>
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+          <Feeds />
+          <Widgets />
+        </div>
+      </div>)}
     </div>
     
   );
---
+++ src/components/Login.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "./styles/Login.css";
+import LinkedInLogo from "../images/Linkedin-Logo.png"
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
+
+
+
+function Login({ login }) {
+  const [name, setName] = useState("");
+  const [profile, setProfile] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        dispatch(login({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          displayName: userAuth.user.displayName,
+          profileUrl: userAuth.user.photoURL,
+        }));
+      })
+      .catch((error) => alert(error));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    if (!name || !email) {
+      return alert("Please enter required fields");
+    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profile,
+          })
+          .then(() => {
+            dispatch(login({
+              email: userAuth.user.email,
+              uid: userAuth.user.uid,
+              displayName: name,
+              photoURL: profile,
+            }));
+          });
+      })
+      .catch((error) => alert(error));
+  };
+  return (
+    <div className="login">
+      <img
+        src={LinkedInLogo}
+        height="150px"
+        width="250px"
+        alt=""
+      />
+      <form>
+        <input
+          type="text"
+          placeholder="Full Name(Required if registering)"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Profile pic URL (optional)"
+          value={profile}
+          onChange={(e) => {
+            setProfile(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          placeholder="Email (required)"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password (required)"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+        <p>
+          Not a member? <span onClick={register}>Register Now</span>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
